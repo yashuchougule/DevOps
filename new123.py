b012b8eba55f00e5342426073c17104725d85277
Yash Chougule <109961491+yashuchougule@users.noreply.github.com>
2026-03-06 12:12:39 +0530

    Add files via upload

---
+++ new123.py
@@ -0,0 +1,8 @@
+var data = Seq(('James','','Smith','1991-04-01','M',3000),
+  ('Michael','Rose','','2000-05-19','M',4000),
+  ('Robert','','Williams','1978-09-05','M',4000),
+  ('Maria','Anne','Jones','1967-12-01','F',4000),
+  ('Jen','Mary','Brown','1980-02-17','F',-1)
+)
+#val columns = Seq("firstname","middlename","lastname","dob","gender","salary")
+#df = spark.createDataFrame(data), schema = columns).toDF(columns:_*)
